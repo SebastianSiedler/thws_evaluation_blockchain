6c2b2f73323aca9a15139f8205961402c8f2ca86
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2024-11-25 16:33:04 +0100

    feat(web): save identity in localStorage

--- web/src/stores/evaluationStore.ts
+++ web/src/stores/evaluationStore.ts
@@ -1,9 +1,18 @@
 import { Identity } from '@semaphore-protocol/core';
+import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { Ref, ref } from 'vue';
 
 export const useEvaluationStore = defineStore('evaluation', () => {
-  const _identity: Ref<Identity> = ref<Identity>(new Identity());
+  const _identity = useLocalStorage<Identity>(
+    'semaphore-identity-pk',
+    new Identity(),
+    {
+      serializer: {
+        read: (v) => Identity.import(v),
+        write: (v) => v.export(),
+      },
+    },
+  );
 
   return {
     _identity,
